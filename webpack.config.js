80f1558951432ddec54a577d900fdb483d1b72ff
Silvio Iannone <silvio@bloom.se>
2017-11-07 15:03:08 +0100

    Include the build process

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path'),
+      nodeEnvFile = require('node-env-file'),
+      webpack = require('webpack');
+
+nodeEnvFile('./.env');
+
+let chunkFilename = (process.env.APP_ENV === 'local') ? 'js/[name].js' : 'js/[name].[chunkhash].js';
+
+module.exports = {
+    resolve: {
+        modules: [
+            path.resolve('./resources'),
+            path.resolve('./node_modules')
+        ]
+    },
+    output: {
+        chunkFilename
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            APP_ENV: JSON.stringify(process.env.APP_ENV),
+            APP_LOG_LEVEL: JSON.stringify(process.env.APP_LOG_LEVEL),
+            APP_NAME: JSON.stringify(process.env.APP_NAME),
+            APP_DOMAIN: JSON.stringify(process.env.APP_DOMAIN),
+            APP_URL: JSON.stringify(process.env.APP_URL),
+            GOOGLE_API_KEY: JSON.stringify(process.env.GOOGLE_API_KEY),
+            SENTRY_DSN_JS: JSON.stringify(process.env.SENTRY_DSN_JS),
+            WEBSOCKET_SERVER_HOST: JSON.stringify(process.env.WEBSOCKET_SERVER_HOST),
+            WEBSOCKET_SERVER_PORT: JSON.stringify(process.env.WEBSOCKET_SERVER_PORT),
+            WEBSOCKET_SERVER_APP_ID: JSON.stringify(process.env.WEBSOCKET_SERVER_APP_ID),
+            WEBSOCKET_SERVER_KEY: JSON.stringify(process.env.WEBSOCKET_SERVER_KEY),
+            WEB_CLIENT_ID: JSON.stringify(process.env.WEB_CLIENT_ID),
+            WEB_CLIENT_SECRET: JSON.stringify(process.env.WEB_CLIENT_SECRET),
+        })
+    ]
+};
---
+++ webpack.mix.js
@@ -0,0 +1,44 @@
+const config = require('spa-skeleton/webpack.config');
+
+module.exports = {
+
+    build: function (mix)
+    {
+        // Load the Configuration from SPA-Skeleton
+        mix.webpackConfig(config);
+
+        mix.js('resources/assets/js/App.js', './public/js/app.js')
+            .extract([
+                'vue',
+                'vue-router',
+                'vuex',
+                'vuetify',
+                'vee-validate',
+                'loglevel',
+                'moment',
+                'lodash',
+                'raven-js'
+            ], './public/js/vendor.js');
+
+        mix.stylus('resources/assets/stylus/app.styl', 'public/css');
+
+        mix.styles([
+            'node_modules/material-design-icons/iconfont/material-icons.css',
+            'node_modules/mdi/css/materialdesignicons.css',
+            'public/css/app.css'
+        ], 'public/css/all.css');
+
+        if (process.env.APP_ENV !== 'local')
+        {
+            mix.version();
+        }
+
+        mix.copy('node_modules/material-design-icons/iconfont', 'public/css');
+        mix.copy('node_modules/mdi/fonts', 'public/fonts');
+
+        if (process.env.APP_ENV !== 'production')
+        {
+            mix.sourceMaps();
+        }
+    }
+}
