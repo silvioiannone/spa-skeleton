22dc216ab03d831d3dd2ed1dcf80c9a2636a1e81
Silvio Iannone <silvio@bloom.se>
2017-11-16 14:38:20 +0100

    Fix the state machine modules import

--- src/Library/App/StateMachine.js
+++ src/Library/App/StateMachine.js
@@ -52,13 +52,19 @@ export default class State
         // Register all the modules with the state machine
         let availableModules = {};
         let vuexModules = {};
-        Object.assign(availableModules, SkeletonModules, Modules);
 
-        for(let key in availableModules)
-        {
-            vuexModules[key] = (new availableModules[key]).get();
+        for (let key in SkeletonModules) {
+            let module = (new SkeletonModules[key]()).get();
+
+            // See if there are app modules defined with the same key. If that's the case we need to
+            // merge the app module with its skeleton module counterpart.
+            if (typeof Modules[key] !== 'undefined') {
+                let appModule = (new Modules[key]());
+                module = Object.assign(module, appModule.get());
+            }
 
             Log.debug('State machine "' + key + '" module registered.');
+            vuexModules[key] = module;
         }
 
         return new Vuex.Store({ modules: vuexModules });
