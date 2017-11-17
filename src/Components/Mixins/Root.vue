52e52eff360768d3f9986401d08280b32696ed75
Silvio Iannone <silvio@bloom.se>
2017-11-17 10:51:20 +0100

    Fix wrong indentation

--- src/Components/Mixins/Root.vue
+++ src/Components/Mixins/Root.vue
@@ -32,13 +32,14 @@
             userLanguage()
             {
                 // Request a new locale file from the server...
-                api.app.getLocale(this.userLanguage)
-                .then(response =>
-                {
-                    // ... and then switch the locale.
-                    this.$i18n.setLocaleMessage(this.userLanguage, response.body);
-                    this.$i18n.locale = this.userLanguage;
-                });
+                api.app
+                    .getLocale(this.userLanguage)
+                    .then(response =>
+                    {
+                        // ... and then switch the locale.
+                        this.$i18n.setLocaleMessage(this.userLanguage, response.body);
+                        this.$i18n.locale = this.userLanguage;
+                    });
             }
         }
     }
