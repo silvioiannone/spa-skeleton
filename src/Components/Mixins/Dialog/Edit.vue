9049db389469e14dc436b0f58b7c1fcb86087423
Silvio Iannone <silvio@bloom.se>
2017-11-27 15:23:13 +0100

    Add an edit dialog mixin

---
+++ src/Components/Mixins/Dialog/Edit.vue
@@ -0,0 +1,38 @@
+<script>
+
+    /*
+     * This mixin adds some functionality to dialogs that are only supposed to display an edit form.
+     */
+    export default {
+
+        name: 'DialogEdit',
+
+        props: {
+
+            /**
+             * Dialog's visibility.
+             */
+            value: {
+                type: Boolean,
+                default: true
+            }
+        },
+
+        computed: {
+
+            /**
+             * Determines the dialog's visibility.
+             */
+            model: {
+                get() {
+                    return this.value;
+                },
+                set(value) {
+                    this.$emit('input', value);
+                    this.$emit('dialog-hidden');
+                }
+            }
+        }
+    }
+
+</script>
