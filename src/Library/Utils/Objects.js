3a0cd61ce5eeead9ebc68cf3597dba3cc918626e
Silvio Iannone <silvio@bloom.se>
2017-11-27 15:22:54 +0100

    Add an object utility function

--- src/Library/Utils/Objects.js
+++ src/Library/Utils/Objects.js
@@ -1,7 +1,29 @@
+import _ from 'lodash';
+
 /**
  * Object utilities.
  */
 export default {
+
+    /**
+     * Pluck a list of unique items from a collection.
+     *
+     * This only works for collection of items returned by the API.
+     *
+     * @param {array} subject
+     * @param {String} key
+     */
+    pluckUnique(subject, key)
+    {
+        let uniques = _(_.map(subject, key)).compact();
+
+        if (Array.isArray(uniques.value()[0])) {
+            uniques = uniques.flatten();
+        }
+
+        return uniques.uniqBy(item => item.id)
+            .value();
+    },
 
     /**
      * Search the subject in an array of objects.
