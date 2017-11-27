4b1650d77d137ed5600f4f6c12adffe4c7c5d14c
Silvio Iannone <silvio@bloom.se>
2017-11-27 15:22:43 +0100

    Add a string utility function

--- src/Library/Utils/String.js
+++ src/Library/Utils/String.js
@@ -40,5 +40,20 @@ export default
         format = format || 'lll';
 
         return Moment(string).format(format);
+    },
+
+    /**
+     * Return the parent path of the given path.
+     *
+     * @param {String} path
+     * @param {Number} levels Defaults to 1.
+     * @return {String}
+     */
+    parentPath(path, levels)
+    {
+        levels = levels || 1;
+
+        let parent = path.split('/');
+        return '/' + parent.splice(1, parent.length - levels - 1).join('/');
     }
 }
