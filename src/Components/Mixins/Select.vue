3a72f5e943b20bc7b408ab2a60cd76ac261bfd50
Silvio Iannone <silvio@bloom.se>
2017-11-29 15:25:55 +0100

    Wrap the v-select Vuetify component and fix issues in the Select mixin

---
+++ src/Components/Mixins/Select.vue
@@ -0,0 +1,135 @@
+<script>
+
+    /**
+     * This mixin can be used in order to bootstrap the creation of a select box.
+     *
+     * If the models that you want to be available in the selectbox do not have a value/text
+     * properties you can create a `transformModel` in the component using this mixin that will
+     * add those two properties to each model. For example:
+     *
+     *     transformModel(model)
+     *     {
+     *         model.text = model.first_name + ' ' + model.last_name;
+     *         model.value = model.id;
+     *
+     *         return model;
+     *     }
+     */
+    export default {
+
+        props: {
+
+            /**
+             * Display chips in the selectbox.
+             */
+            chips: {
+                type: Boolean,
+                default: false
+            },
+
+            /**
+             * Whether or not to display a button to clean the selectbox.
+             */
+            clearable: {
+                type: Boolean,
+                default: false
+            },
+
+            /**
+             * Selectbox label.
+             */
+            label: {
+                type: String,
+                default: 'Users'
+            },
+
+            /**
+             * Models that will be available in the selectbox.
+             */
+            models: {
+                type: Array,
+                required: true
+            },
+
+            /**
+             * Already selected subjects.
+             *
+             * If it is an array then multiple elements will be allowed to be selected.
+             */
+            selected: {},
+
+            /**
+             * Model transformation function.
+             */
+            transformation: {
+                type: Function,
+                default: () => {}
+            }
+        },
+
+        data()
+        {
+            return {
+                _selected: [],
+                _models: [],
+                value: null
+            }
+        },
+
+        computed: {
+
+            multiple()
+            {
+                return Array.isArray(this.selected);
+            }
+        },
+
+        methods: {
+
+            /**
+             * Fire the needed event.
+             */
+            fire(models)
+            {
+                if (models === null) {
+                    models = { id: null }
+                }
+                this.$emit('selected', models);
+            },
+
+            /**
+             * Transform a model so that it can be used by the select box.
+             */
+            _transformModel(model)
+            {
+                if (typeof this.transformModel !== 'undefined') {
+                    let localModel = JSON.parse(JSON.stringify(model));
+
+                    if (localModel) {
+                        return this.transformModel(localModel)
+                    }
+                }
+
+                return model;
+            }
+        },
+
+        created()
+        {
+            this.transformModel = this.transformation;
+        },
+
+        mounted()
+        {
+            if (this.multiple) {
+                this.$data._selected = this.selected.map(model => this._transformModel(model));
+            } else {
+                this.$data._selected = this._transformModel(this.selected);
+            }
+
+            this.$data.value = this.$data._selected;
+            this.$data._models = this.models.map(model => this._transformModel(model));
+        }
+    }
+
+</script>
---
+++ src/Components/Select/Main.vue
@@ -0,0 +1,50 @@
+<script>
+
+    import Select from '../Mixins/Select.vue';
+
+    export default {
+
+        name: 'SelectMain',
+
+        mixins: [
+            Select
+        ],
+
+        computed: {
+
+            hasSlots()
+            {
+                return !!this.$scopedSlots.selection || !!this.$scopedSlots.item;
+            }
+        },
+
+        render(createElement)
+        {
+            let self = this;
+
+            return createElement(
+                'v-select',
+                {
+                    props: {
+                        autocomplete: true,
+                        chips: this.chips,
+                        clearable: this.clearable,
+                        items: this.$data._models,
+                        label: this.label,
+                        multiple: this.multiple,
+                        returnObject: true,
+                        value: this.$data._selected
+                    },
+                    on: {
+                        input: value => {
+                            //this.$emit('selected', value);
+                            this.fire(value)
+                        }
+                    },
+                    scopedSlots: this.$vnode.data.scopedSlots
+                }
+            )
+        }
+    }
+
+</script>
