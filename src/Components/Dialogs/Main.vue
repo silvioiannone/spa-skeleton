47ef9853b4bbfff0aaf56f073ef8b4f4c6f45deb
Silvio Iannone <silvio@bloom.se>
2017-11-30 13:15:45 +0100

    Add a dialog component

---
+++ src/Components/Dialogs/Main.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-dialog v-model="value" max-width="500">
+        <slot></slot>
+    </v-dialog>
+</template>
+
+<script>
+
+    export default {
+
+        name: 'DialogMain',
+
+        props: {
+
+            value: {
+                type: Boolean,
+                default: false
+            }
+        },
+
+        watch: {
+
+            value(value)
+            {
+                this.$emit('input', value);
+            }
+        }
+    }
+
+</script>
