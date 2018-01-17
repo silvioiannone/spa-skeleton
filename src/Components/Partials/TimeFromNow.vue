897ce6436fc0f82cfca39bd78a19d3befbae4003
Silvio Iannone <silvio@bloom.se>
2018-01-17 14:48:43 +0100

    Add a component that displayed the time that has passed from a date

---
+++ src/Components/Partials/TimeFromNow.vue
@@ -0,0 +1,45 @@
+<template>
+    <span>{{ $data._time }}</span>
+</template>
+
+<script>
+
+    import Moment from 'moment';
+
+    export default {
+
+        name: 'TimeFromNow',
+
+        props: {
+
+            /**
+             * The time that will be displayed in a human readable format.
+             */
+            time: {
+                type: String,
+                required: true
+            }
+        },
+
+        data()
+        {
+            return {
+                _time: ''
+            }
+        },
+
+        methods: {
+
+            update()
+            {
+                this.$data._time = Moment.utc(this.time).local().fromNow();
+            }
+        },
+
+        mounted()
+        {
+            setInterval(this.update, 1000);
+        }
+    }
+
+</script>
