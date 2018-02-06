20d269ac4101af818a5170873c6e40914c1e451a
Silvio Iannone <silvio@bloom.se>
2018-02-06 16:08:00 +0100

    Fix an issue causing the errors to not be styled correctly

--- src/Components/Views/Errors/Main.vue
+++ src/Components/Views/Errors/Main.vue
@@ -1,25 +1,33 @@
 <template>
-    <v-container>
-        <v-layout>
-            <slot></slot>
-        </v-layout>
-        <v-layout>
-            <v-flex xs12>
-                <div class="text-xs-center">
-                    <v-btn color="primary" large @click="goBack()" v-if="backButton">
-                        <v-icon left>arrow_back</v-icon>Go back
-                    </v-btn>
-                </div>
-            </v-flex>
-        </v-layout>
-    </v-container>
+    <layout-app>
+        <v-container>
+            <v-layout>
+                <slot></slot>
+            </v-layout>
+            <v-layout>
+                <v-flex xs12>
+                    <div class="text-xs-center">
+                        <v-btn color="primary" large @click="goBack()" v-if="backButton">
+                            <v-icon left>arrow_back</v-icon>Go back
+                        </v-btn>
+                    </div>
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </layout-app>
 </template>
 
 <script>
 
+    import LayoutApp from 'spa-skeleton/src/Components/Layouts/App'
+
     export default
     {
         name: 'Error',
+
+        components: {
+            LayoutApp
+        },
 
         props: {
 
