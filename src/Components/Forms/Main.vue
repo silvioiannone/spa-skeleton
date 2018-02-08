151c64343b25b30f8111965b368d165bf209edb6
Silvio Iannone <silvio@bloom.se>
2018-02-08 15:42:31 +0100

    Chanche approach for centering the form buttons

--- src/Components/Forms/Main.vue
+++ src/Components/Forms/Main.vue
@@ -2,13 +2,15 @@
     <v-form @submit.prevent>
         <v-container fluid grid-list-lg class="pa-0">
             <slot></slot>
-            <v-layout row :justify-space-around="centerActions">
-                <slot name="actions"></slot>
-                <v-btn v-if="_cancellable" flat @click="cancel">Cancel</v-btn>
-                <button-submit :on-click="handleSubmit" color="primary"
-                               :disabled="$parent.errors.any() || !canSubmit">
-                    {{ submitText }}
-                </button-submit>
+            <v-layout row>
+                <v-flex :class="{'text-xs-center': centerActions}">
+                    <slot name="actions"></slot>
+                    <v-btn v-if="_cancellable" flat @click="cancel">Cancel</v-btn>
+                    <button-submit :on-click="handleSubmit" color="primary"
+                                   :disabled="$parent.errors.any() || !canSubmit">
+                        {{ submitText }}
+                    </button-submit>
+                </v-flex>
             </v-layout>
         </v-container>
     </v-form>
