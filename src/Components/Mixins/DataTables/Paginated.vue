32d1f9e5553dfe4891b58216653445f0787f5c51
Silvio Iannone <silvio@bloom.se>
2018-02-22 08:51:11 +0100

    Add a pagination mixin for data tables

---
+++ src/Components/Mixins/DataTables/Paginated.vue
@@ -0,0 +1,37 @@
+<script>
+
+    /**
+     * This mixin allows data tables to behave correctly when dealing with paginated data.
+     *
+     * It should be used by all tables that display paginated data.
+     */
+
+    export default {
+
+        props: {
+
+            /**
+             * Pagination meta.
+             */
+            pagination: {
+                type: Object,
+                required: true
+            }
+        },
+
+        data()
+        {
+            return {
+                _pagination: {}
+            }
+        },
+
+        mounted()
+        {
+            this.$data._pagination = this.pagination;
+
+            this.$watch(() => this.$data._pagination, value => this.$emit('update:pagination', value));
+        }
+    }
+
+</script>
