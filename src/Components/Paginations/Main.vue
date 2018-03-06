4b2021caafb8bbf1e18ab8a86bc3de0845a488ce
Silvio Iannone <silvio@bloom.se>
2018-03-06 14:19:43 +0100

    Add a pagination component

---
+++ src/Components/Paginations/Main.vue
@@ -0,0 +1,44 @@
+<template>
+    <v-pagination :length="pagination.totalPages" v-model="page" :total-visible="7">
+    </v-pagination>
+</template>
+
+<script>
+
+    export default {
+
+        name: 'PaginationMain',
+
+        data()
+        {
+            return {
+                page: 1
+            }
+        },
+
+        props: {
+
+            /**
+             * Pagination object.
+             */
+            pagination: {
+                type: Object,
+                required: true
+            }
+        },
+
+        mounted()
+        {
+            this.page = this.pagination.page;
+
+            this.$watch('page', () =>
+            {
+                this.$emit('update:pagination', {
+                    ...this.pagination,
+                    page: this.page
+                });
+            })
+        }
+    }
+
+</script>
