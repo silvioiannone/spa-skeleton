fea9b40921cfa32fc5f4d91de88b9260843e0bde
Silvio Iannone <silvio@bloom.se>
2018-03-16 09:12:44 +0100

    Add a form card container mixin

---
+++ src/Components/Mixins/Containers/Cards/Form.vue
@@ -0,0 +1,32 @@
+<script>
+
+    /*
+     * This mixin is intented to be used with cards holding forms.
+     */
+    export default {
+
+        methods: {
+
+            /**
+             * Propagates the cancel form event to the parent.
+             *
+             * @param event
+             */
+            bubbleCancelledEvent(event)
+            {
+                this.$emit('cancel', event);
+            },
+
+            /**
+             * Propagates the submitted form event to the parent.
+             *
+             * @param event
+             */
+            bubbleSubmittedEvent(event)
+            {
+                this.$emit('submitted', event)
+            }
+        }
+    }
+
+</script>
