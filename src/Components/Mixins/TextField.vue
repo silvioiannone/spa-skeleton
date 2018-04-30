eb7807d6879f5ee4688f4785d0af4b149b065c28
Silvio Iannone <silvio@bloom.se>
2018-04-30 14:51:47 +0200

    Add a mixin that can be used to bootstrap text field components

---
+++ src/Components/Mixins/TextField.vue
@@ -0,0 +1,158 @@
+<script>
+
+    /**
+     * This mixin can be used in order to create text fields.
+     *
+     * It is possible to override any of the value of the props by defining a computed value with
+     * the same name.
+     */
+    export default {
+
+        props: {
+
+            /**
+             * Label.
+             */
+            label: {
+                type: String,
+                default: ''
+            },
+
+            /**
+             * A hint displayed under the text field.
+             */
+            hint: {
+                type: String,
+                default: ''
+            },
+
+            /**
+             * Make the hint always visible.
+             */
+            persistentHint: {
+                type: Boolean,
+                default: false
+            },
+
+            /**
+             * Make the phone input required.
+             */
+            required: {
+                type: Boolean,
+                default: true
+            },
+
+            /**
+             * Value of the data-vv-name attribute.
+             */
+            dataVvName: {
+                type: String,
+                default: ''
+            },
+
+            /**
+             * Validation rules.
+             */
+            validation: {
+                type: [String, Object],
+                default: ''
+            },
+
+            /**
+             * Input type.
+             */
+            type: {
+                type: String,
+                default: ''
+            },
+
+            /**
+             * Model value.
+             */
+            value: {}
+        },
+
+        data()
+        {
+            return {
+                _value: this.value
+            }
+        },
+
+        computed: {
+
+            errorMessages()
+            {
+                return this.errors.collect(this.dataVvName);
+            }
+        },
+
+        methods: {
+
+            /**
+             * Bubble the input event.
+             *
+             * This method is declared here instead of the event definition because this way it can
+             * be easily overridden.
+             *
+             * @param value
+             */
+            fireInputEvent(value) {
+                this.$emit('input', value);
+            }
+        },
+
+        watch: {
+
+            value()
+            {
+                this.$nextTick(() => this.$validator.validateAll());
+            }
+        },
+
+        render(createElement)
+        {
+            let self = this;
+
+            let props = Object.assign({}, this.$props, {
+                errorMessages: this.errorMessages
+            });
+
+            return createElement('v-text-field', {
+                attrs: {
+                    'data-vv-name': this.dataVvName,
+                    'type': this.type
+                },
+                directives: [
+                    {
+                        name: 'validate',
+                        value: this.validation
+                    }
+                ],
+                props,
+                on: {
+                    input: value => self.fireInputEvent(value),
+                    blur: () => self.$emit('blur'),
+                    focus: () => self.$emit('focus'),
+                    'update:error': value => {
+                        let formContainer = this.$parent.$parent.$parent;
+                        formContainer.$validator.errors.remove(this.dataVvName);
+
+                        if (value) {
+                            // Take the error and assign it to the parent validator.
+                            // The first parent is VForm.
+                            // The second parent is FormMain.
+                            // The third parent is the actual form container component.
+                            this.$validator.errors.items.forEach(error =>
+                            {
+                                formContainer.$validator.errors.add(error);
+                            });
+                        }
+                        self.$emit('update:error')
+                    }
+                }
+            });
+        }
+    }
+
+</script>
