2005fca5d9b9d9fa4490e351f278889e6e94e992
Silvio Iannone <silvio@bloom.se>
2018-05-08 15:32:07 +0200

    Add an updated login form

---
+++ src/Components/Forms/Login.vue
@@ -0,0 +1,61 @@
+<template>
+    <form-main :submit="login" submit-text="login">
+        <v-flex xs12>
+            <v-text-field label="E-mail" v-model="model.username"></v-text-field>
+        </v-flex>
+        <v-flex xs12>
+            <v-text-field label="Password" type="password" v-model="model.password"></v-text-field>
+        </v-flex>
+    </form-main>
+</template>
+
+<script>
+
+    import MixinContainerForm from 'spa-skeleton/src/Components/Mixins/Containers/Form';
+
+    export default {
+
+        name: 'FormLogin',
+
+        mixins: [
+            MixinContainerForm
+        ],
+
+        data()
+        {
+            return {
+                model: {
+                    username: '',
+                    password: ''
+                }
+            }
+        },
+
+        methods: {
+
+            /**
+             * Authenticate the user.
+             */
+            login()
+            {
+                return new Promise((resolve, reject) =>
+                {
+                    this.$api.users.login(this.model)
+                        .then(response =>
+                        {
+                            this.$router.push('/home');
+                            resolve(response);
+                        })
+                        .catch(response => reject(response));
+                });
+            }
+        },
+
+        mounted()
+        {
+            this.$set(this.model, 'username', 'user@boptimera.se');
+            this.$set(this.model, 'password', 'password');
+        }
+    }
+
+</script>
