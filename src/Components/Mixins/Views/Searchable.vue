516c9dfba5c21bc23ab644177ee5896949d68620
Silvio Iannone <silvioiannone91@gmail.com>
2018-07-31 09:31:31 +0200

    Add a searchable view mixin

---
+++ src/Components/Mixins/Views/Searchable.vue
@@ -0,0 +1,40 @@
+<script>
+
+    let timeout = null;
+
+    export default {
+
+        methods: {
+
+            /**
+             * Perform a server-side search.
+             *
+             * @param subject
+             */
+            search(subject)
+            {
+                if (timeout) {
+                    clearTimeout(timeout);
+                }
+
+                timeout = setTimeout(() =>
+                {
+                    let query = {
+                        ...this.$route.query,
+                        search: subject
+                    };
+
+                    if (!subject) {
+                        delete query['search'];
+                    }
+
+                    this.$router.push({
+                        path: this.$route.path,
+                        query
+                    });
+                }, 500)
+            }
+        }
+    }
+
+</script>
