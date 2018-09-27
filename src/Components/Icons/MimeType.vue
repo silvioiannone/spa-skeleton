a798f11f77c9aa9dfd453d84ae126e33388642f4
Silvio Iannone <silvioiannone91@gmail.com>
2018-09-27 09:54:00 +0200

    Add a component that display an icon related to a mime type

---
+++ src/Components/Icons/MimeType.vue
@@ -0,0 +1,49 @@
+<template>
+    <v-icon>{{ icon }}</v-icon>
+</template>
+
+<script>
+
+    export default {
+
+        name: 'IconMimeType',
+
+        props: {
+
+            /**
+             * Mime type.
+             */
+            mime: {
+                type: String,
+                required: true
+            }
+        },
+
+        data()
+        {
+            return {
+                mimeTypes: {},
+                categoryToIcon: {
+                    application: 'mdi-application',
+                    audio: 'mdi-file-music',
+                    image: 'mdi-file-image',
+                    text: 'mdi-file-document',
+                    video: 'mdie-file-video'
+                }
+            }
+        },
+
+        computed: {
+
+            icon()
+            {
+                let result = /(.+)\/(.+)/.exec(this.mime);
+                let category = result[1];
+                let subtype = result[2];
+
+                return this.categoryToIcon[category] || 'mdi-file';
+            }
+        }
+    }
+
+</script>
