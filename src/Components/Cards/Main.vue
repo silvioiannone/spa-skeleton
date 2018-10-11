9dc46d39dd95c8a332b6b9c961bfdedcd1f77e85
Silvio Iannone <silvioiannone91@gmail.com>
2018-10-11 11:11:22 +0200

    Add a main card component

---
+++ src/Components/Cards/Main.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-card>
+        <v-card-title class="headline" v-if="title.length">{{ title }}</v-card-title>
+        <slot></slot>
+    </v-card>
+</template>
+
+<script>
+
+    export default {
+
+        name: 'CardMain',
+
+        props: {
+
+            /**
+              * Title displayed by the card.
+              */
+            title: {
+                type: String,
+                default: ''
+            }
+        }
+    }
+
+</script>
