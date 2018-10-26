2a771e3e8ca2244b73ddf36ef67d2d84cc5fa36b
Silvio Iannone <silvioiannone91@gmail.com>
2018-10-26 11:39:19 +0200

    Add a breadcrumbs main component

---
+++ src/Components/Breadcrumbs/Main.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-breadcrumbs :divider="divider" :items="_items"></v-breadcrumbs>
+</template>
+
+<script>
+
+    import Breadcrumbs from '../Mixins/Breadcrumbs';
+
+    export default {
+
+        name: 'BreadcrumbsMain',
+
+        mixins: [
+            Breadcrumbs
+        ],
+
+        computed: {
+
+            _items()
+            {
+                return this.items.map(item =>
+                {
+                    return {
+                        disabled: ! item.to,
+                        exact: true,
+                        ...item,
+                    }
+                });
+            }
+        }
+    }
+
+</script>
---
+++ src/Components/Mixins/Breadcrumbs.vue
@@ -0,0 +1,25 @@
+<script>
+
+    export default {
+
+        props: {
+
+            /**
+             * An array containing the items that will be displayed.
+             */
+            items: {
+                type: Array,
+                default: () => []
+            },
+
+            /**
+             * Specifies the dividing character.
+             */
+            divider: {
+                type: String,
+                divider: '/'
+            }
+        }
+    }
+
+</script>
