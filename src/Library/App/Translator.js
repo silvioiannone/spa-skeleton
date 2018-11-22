7cf3955ba825196323063d2bb13129eff9681de5
Silvio Iannone <silvioiannone91@gmail.com>
2018-11-22 15:21:07 +0100

    Fix an issue causing the translator to ignore the skeleton locales

--- src/Library/App/Translator.js
+++ src/Library/App/Translator.js
@@ -1,6 +1,7 @@
-import Log from 'loglevel';
+import _       from 'lodash';
+import Log     from 'loglevel';
 import VueI18N from 'vue-i18n';
-import Config from '../../Config';
+import Config  from '../../Config';
 
 /**
  * Add multiple language support to the app.
@@ -12,15 +13,17 @@ export default class Translator
      *
      * @returns {Translator}
      */
-    boot ()
+    boot()
     {
         Log.debug('Loading language...');
 
         let locale = Config.locale;
-        let defaultLocale = require('locales/' + locale + '.json');
+        let appLocale = require('locales/' + locale + '.json');
+        let skeletonLocale = require('./../../Assets/Locales/' + locale + '.json');
+
         let messages = {};
 
-        messages[locale] = defaultLocale;
+        messages[locale] = _.merge(skeletonLocale, appLocale);
 
         Log.debug('Language loaded.');
 
