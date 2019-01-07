dcdf9cf5f1b6b91e0e0a2956fb622499e99aff57
Silvio Iannone <silvioiannone91@gmail.com>
2019-01-07 11:33:41 +0100

    Start defining the exports of the `index.ts` file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
-import App from './App';
+export { default as Config } from './Config';
+export { default as Api } from './Library/Api';
+export { default as ResponseInterface } from './Library/API/ResponseInterface';
 
+/*
+ * Models.
+ */
+import NotificationModel from './Library/App/State/Models/Notification';
+
+export namespace Models {
+    export class Notification extends NotificationModel {}
+}
+
+import App from './App';
 export default App;
-export * from './Config';
