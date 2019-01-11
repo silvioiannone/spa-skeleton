8780dc8ad7890ddf53c6d04d1b268ec17521bb06
Silvio Iannone <silvioiannone91@gmail.com>
2019-01-11 16:45:22 +0100

    Add a `title-actions` slot to the `card-main` component

--- src/Components/Cards/Main.vue
+++ src/Components/Cards/Main.vue
@@ -1,6 +1,10 @@
 <template>
     <v-card>
-        <v-card-title class="headline" v-if="title.length">{{ title }}</v-card-title>
+        <v-card-title class="headline" v-if="title.length">
+            {{ title }}
+            <v-spacer></v-spacer>
+            <slot name="title-actions"></slot>
+        </v-card-title>
         <slot></slot>
     </v-card>
 </template>
