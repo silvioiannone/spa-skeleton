cfeae436f842a81bef6d16a6b547a392493f162d
Silvio Iannone <silvioiannone91@gmail.com>
2019-01-16 10:43:40 +0100

    Allow API calls to be performed from the models
    This is needed in order to facilitate the synchronisation between the API and the state machine.

--- src/Library/Services/StateMachine/VuexOrm/Support/ExtendedModel.ts
+++ src/Library/Services/StateMachine/VuexOrm/Support/ExtendedModel.ts
@@ -4,6 +4,8 @@ import { CreateParameters } from '../Plugins/ApiSync/Actions/Create';
 import { GetParameters }    from '../Plugins/ApiSync/Actions/Get';
 import { UpdateParameters } from '../Plugins/ApiSync/Actions/Update';
 import { DeleteParameters } from '../Plugins/ApiSync/Actions/Delete';
+import { ApiClient,
+    default as ApiFactory } from '../../../../Api';
 
 /**
  * This class provides support for the ApiSync plugin.
@@ -64,5 +66,30 @@ export class ExtendedModel extends Model
             options,
             model: this
         });
+    }
+
+    /**
+     * Make an API call.
+     */
+    static async api(
+        execute: (client: ApiClient) => Promise<ResponseInterface>
+    ): Promise<ResponseInterface>
+    {
+        let api = ApiFactory.make();
+        let promise = execute(api);
+
+        promise.then(response =>
+        {
+            if (response.body.meta) {
+                ExtendedModel.store()
+                   .commit('app/insert', {
+                       ui: {
+                           pagination: response.body.meta
+                       }
+                   });
+            }
+        });
+
+        return promise;
     }
 }
