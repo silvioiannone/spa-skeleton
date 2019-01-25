02a4e2bec37994fea8ff72957319da9d9667c7f5
Silvio Iannone <silvioiannone91@gmail.com>
2019-01-25 16:20:37 +0100

    Export additional components and mixins

--- src/index.ts
+++ src/index.ts
@@ -21,12 +21,17 @@ export { default as CardMain } from './Components/Cards/Main.vue';
 export { default as DataTableMain } from './Components/DataTables/Main.vue';
 export { default as DialogMain } from './Components/Dialogs/Main.vue';
 export { default as FormMain } from './Components/Forms/Main.vue';
+export { default as LayoutApp } from './Components/Layouts/App.vue';
 export { default as NavigationDrawerMain } from './Components/NavigationDrawers/Main.vue';
+export { default as NavigationDrawerNotifications } from './Components/NavigationDrawers/Notifications.vue';
+export { default as ResponsiveContainer } from './Components/Misc/Grid/ResponsiveContainer.vue';
 export { default as ToolbarMain } from './Components/Toolbars/Main.vue';
 
 /**
  * Mixins
  */
+export { default as ViewFilterable } from './Components/Mixins/Views/Filterable.vue';
+export { default as ViewSearchable } from './Components/Mixins/Views/Searchable.vue';
 export { default as ViewPaginated } from './Components/Mixins/Views/Paginated.vue';
 export { default as Subscribable } from './Components/Mixins/Subscribable.vue';
 export { default as Wrapper } from './Components/Mixins/Wrapper.vue';
