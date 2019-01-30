2f513709e836aa25e2c347c77554c5a547d10696
Silvio Iannone <silvioiannone91@gmail.com>
2019-01-30 16:35:48 +0100

    Add a dialog form component

---
+++ src/Components/Dialogs/Form.vue
@@ -0,0 +1,25 @@
+<template>
+    <dialog-main v-model="model">
+        <slot></slot>
+    </dialog-main>
+</template>
+
+<script lang="ts">
+
+    import { Component, Mixins, Prop } from 'vue-property-decorator';
+    import DialogMain                  from './Main.vue';
+    import MixinDialogForm             from '../Mixins/Dialog/Form.vue';
+
+    /**
+     * This is a dialog that can hold a form in it.
+     */
+    @Component({
+        components: {
+            DialogMain
+        }
+    })
+    export default class Form extends Mixins(MixinDialogForm)
+    {
+    }
+
+</script>
