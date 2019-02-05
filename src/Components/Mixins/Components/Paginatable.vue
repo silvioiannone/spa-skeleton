4ba7bfe760ac34cdac41ae980290b238208a091d
Silvio Iannone <silvioiannone91@gmail.com>
2019-02-05 11:17:14 +0100

    Add a paginatable component mixin

---
+++ src/Components/Mixins/Components/Paginatable.vue
@@ -0,0 +1,55 @@
+<script lang="ts">
+
+    import Vue                  from 'vue';
+    import { Component, Watch } from 'vue-property-decorator';
+    import PaginationInterface  from '../../../Library/Interfaces/Pagination';
+    import ResponseInterface    from '../../../Library/Api/ResponseInterface';
+    import Pagination           from '../../../Library/Utils/Pagination';
+
+    /**
+     * This mixins handles the pagination and the paginated data displayed by a component.
+     */
+    @Component
+    export default class Paginatable extends Vue
+    {
+        pagination: PaginationInterface = Pagination.initialValue();
+
+        /**
+         * Get the paginated data.
+         *
+         * This function needs to be overridden.
+         */
+        getPaginatedData(paramters: any): Promise<ResponseInterface>
+        {
+            return <Promise<ResponseInterface>><unknown>false;
+        }
+
+        /**
+         * Get the paginated data.
+         */
+        async localGetPaginatedData()
+        {
+            let parameters = Pagination.makeQueryParamsFromVuetifyPagination(this.pagination);
+
+            let response = await this.getPaginatedData(parameters);
+
+            this.pagination = { ...Pagination.makeFromResponse(response) }
+        }
+
+        mounted()
+        {
+            this.localGetPaginatedData();
+        }
+
+        @Watch('pagination')
+        onPaginationChange(oldPagination: PaginationInterface, newPagination: PaginationInterface)
+        {
+            if (JSON.stringify(oldPagination) === JSON.stringify(newPagination)) {
+                return;
+            }
+
+            this.localGetPaginatedData();
+        }
+    }
+
+</script>
