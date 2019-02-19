1bce827942ad55b13d0741601ae5be67aef73a3b
Silvio Iannone <silvioiannone91@gmail.com>
2019-02-19 12:58:47 +0100

    Export the new components

--- index.ts
+++ index.ts
@@ -21,8 +21,29 @@ export namespace Models {
 }
 
 /**
+ * Interfaces.
+ */
+export { InputDescription } from './src/Library/Interfaces/InputDescription';
+
+/**
+ * Mixins
+ */
+export { default as ComponentFilterable } from './src/Components/Mixins/Components/Filterable.vue';
+export { default as ComponentPaginatable } from './src/Components/Mixins/Components/Paginatable.vue';
+export { default as ComponentSearchable } from './src/Components/Mixins/Components/Searchable.vue';
+export { default as Input } from './src/Components/Mixins/Input.vue';
+export { default as TextField } from './src/Components/Mixins/TextField.vue';
+export { default as ViewFilterable } from './src/Components/Mixins/Views/Filterable.vue';
+export { default as ViewSearchable } from './src/Components/Mixins/Views/Searchable.vue';
+export { default as ViewPaginated } from './src/Components/Mixins/Views/Paginated.vue';
+export { default as Subscribable } from './src/Components/Mixins/Subscribable.vue';
+export { default as Wrapper } from './src/Components/Mixins/Wrapper.vue';
+
+/**
  * Components
  */
+export { default as ButtonSubmit } from './src/Components/Buttons/Submit.vue';
+export { default as ButtonConfirm } from './src/Components/Buttons/Confirm.vue';
 export { default as BreadcrumbsMain } from './src/Components/Breadcrumbs/Main.vue';
 export { default as CardForm } from './src/Components/Cards/Form.vue';
 export { default as CardMain } from './src/Components/Cards/Main.vue';
@@ -31,7 +52,9 @@ export { default as DataTableMain } from './src/Components/DataTables/Main.vue';
 export { default as DialogMain } from './src/Components/Dialogs/Main.vue';
 export { default as DialogForm } from './src/Components/Dialogs/Form.vue';
 export { default as FormMain } from './src/Components/Forms/Main.vue';
+export { default as IFrame } from './src/Components/Misc/IFrame.vue';
 export { default as InputAutocomplete } from './src/Components/Inputs/Autocomplete.vue';
+export { default as InputDynamic} from './src/Components/Inputs/Dynamic.vue';
 export { default as LayoutApp } from './src/Components/Layouts/App.vue';
 export { default as NavigationDrawerMain } from './src/Components/NavigationDrawers/Main.vue';
 export { default as NavigationDrawerNotifications } from './src/Components/NavigationDrawers/Notifications.vue';
@@ -40,18 +63,6 @@ export { default as SelectMain } from './src/Components/Select/Main.vue';
 export { default as TextFieldSearch } from './src/Components/TextFields/Search.vue';
 export { default as ToolbarMain } from './src/Components/Toolbars/Main.vue';
 export { default as ViewErrorNotFound } from './src/Components/Views/Errors/NotFound.vue';
-
-/**
- * Mixins
- */
-export { default as ComponentFilterable } from './src/Components/Mixins/Components/Filterable.vue';
-export { default as ComponentPaginatable } from './src/Components/Mixins/Components/Paginatable.vue';
-export { default as ComponentSearchable } from './src/Components/Mixins/Components/Searchable.vue';
-export { default as ViewFilterable } from './src/Components/Mixins/Views/Filterable.vue';
-export { default as ViewSearchable } from './src/Components/Mixins/Views/Searchable.vue';
-export { default as ViewPaginated } from './src/Components/Mixins/Views/Paginated.vue';
-export { default as Subscribable } from './src/Components/Mixins/Subscribable.vue';
-export { default as Wrapper } from './src/Components/Mixins/Wrapper.vue';
 
 import App from './src/App';
 
