8290972af342cf178186bac480c5d50afd1f8ad6
Silvio Iannone <silvioiannone91@gmail.com>
2019-02-19 12:57:31 +0100

    Add the Iframe component

---
+++ src/Components/Misc/IFrame.vue
@@ -0,0 +1,29 @@
+<template>
+    <iframe :frameborder="frameBorder" :src="_content"></iframe>
+</template>
+
+<script lang="ts">
+
+    import Vue                 from 'vue';
+    import { Component, Prop } from 'vue-property-decorator';
+
+    @Component
+    export default class IFrame extends Vue
+    {
+        /**
+         * Frame border attribute.
+         */
+        @Prop({ type: Number, default: 0 }) frameBorder: number;
+
+        /**
+         * Iframe content.
+         */
+        @Prop({ type: String, required: true }) content: string;
+
+        get _content()
+        {
+            return 'data:text/html;charset=utf-8,' + escape(this.content);
+        }
+    }
+
+</script>
