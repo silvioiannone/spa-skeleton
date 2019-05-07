c5073c88ab19c9aebdf4db09878b1e0b81c6ed49
Pavel Mironchik <mironchikpavel@gmail.com>
2019-05-07 23:11:05 +0600

    Port MIME type icon component to TS.

--- src/Components/Icons/MimeType.vue
+++ src/Components/Icons/MimeType.vue
@@ -2,48 +2,40 @@
     <v-icon>{{ icon }}</v-icon>
 </template>
 
-<script>
+<script lang="ts">
 
-    export {
+    import { Component, Prop, Vue } from 'vue-property-decorator';
 
-        name: 'IconMimeType',
+    @Component
+    export class IconMimeType extends Vue
+    {
+        /**
+         * Mime type.
+         */
+        @Prop({ type: String, required: true }) mime: string;
 
-        props: {
+        categoryToIcon = {
+            application: 'mdi-application',
+            audio: 'mdi-file-music',
+            image: 'mdi-file-image',
+            text: 'mdi-file-document',
+            video: 'mdi-file-video'
+        };
 
-            /**
-             * Mime type.
-             */
-            mime: {
-                type: String,
-                required: true
-            }
-        },
-
-        data()
+        get icon(): string
         {
-            return {
-                mimeTypes: {},
-                categoryToIcon: {
-                    application: 'mdi-application',
-                    audio: 'mdi-file-music',
-                    image: 'mdi-file-image',
-                    text: 'mdi-file-document',
-                    video: 'mdie-file-video'
-                }
-            }
-        },
-
-        computed: {
-
-            icon()
-            {
-                let result = /(.+)\/(.+)/.exec(this.mime);
+            let result = /(.+)\/(.+)/.exec(this.mime);
+            if (result) {
                 let category = result[1];
                 let subtype = result[2];
 
                 return this.categoryToIcon[category] || 'mdi-file';
+            } else {
+                return 'mdi-file';
             }
         }
     }
+
+    export default IconMimeType;
 
 </script>
