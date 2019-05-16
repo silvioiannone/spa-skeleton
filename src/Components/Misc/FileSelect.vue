50212c6d9e0fda387f3f704bf6aa7df36cd489f5
Pavel Mironchik <mironchikpavel@gmail.com>
2019-05-16 22:33:01 +0600

    Port file select component to TS.

--- src/Components/Misc/FileSelect.vue
+++ src/Components/Misc/FileSelect.vue
@@ -8,137 +8,127 @@
         </v-flex>
         <v-flex xs12>
             <slot :files="files" :pagination="pagination" :update-pagination="updatePagination"
-                :loading="loading" :selected="selected" :toggle-file="toggleFile">
+                  :loading="loading" :selected="selected" :toggle-file="toggleFile">
             </slot>
         </v-flex>
     </v-layout>
 </template>
 
-<script>
+<script lang="ts">
 
-    import Pagination from 'spa-skeleton/src/Library/Utils/Pagination';
+    import { Component, Prop, Watch, Vue }       from 'vue-property-decorator';
+    import { Pagination }                        from '../../Library/Utils/Pagination';
+    import { Pagination as PaginationInterface } from '../../Library/Interfaces/Pagination';
 
-    let timeout = null;
+    let timeout: any = null;
 
-    export {
+    @Component
+    export class FileSelect extends Vue
+    {
+        /**
+         * API Endpoint (relative to the base URL) that will be used in order to retrieve the
+         * files.
+         *
+         * Example: $api.files
+         */
+        @Prop({type: Object, default: null}) getEndpoint: any;
 
-        name: 'FileSelect',
+        selected: Array<any> = [];
+        searchText = '';
+        loading = false;
+        files: Array<any> = [];
+        pagination: PaginationInterface = {
+            page: 0,
+            rowsPerPage: 0,
+            totalItems: 0,
+            totalPages: 0,
+            sortBy: '',
+            descending: null
+        };
 
-        props: {
-
-            /**
-             * API Endpoint (relative to the base URL) that will be used in order to retrieve the
-             * files.
-             *
-             * Example: $api.files
-             */
-            getEndpoint: {
-                type: Object,
-                default: null
-            }
-        },
-
-        data()
+        get _getEndpoint(): any
         {
-            return {
-                selected: [],
-                searchText: '',
-                loading: false,
-                files: [],
-                pagination: {
-                    sortBy: null
-                }
+            let apiEndpoint = this.getEndpoint || this.$api.files;
+            let parameters = Pagination.makeQueryParamsFromVuetifyPagination(this.pagination);
+
+            if (this.searchText.length) {
+                parameters.search = this.searchText;
             }
-        },
 
-        computed: {
+            return apiEndpoint
+                .setParameters(parameters)
+                .get();
+        }
 
-            _getEndpoint()
-            {
-                let apiEndpoint = this.getEndpoint || this.$api.files;
-                let parameters = Pagination.makeQueryParamsFromVuetifyPagination(this.pagination);
+        /**
+         * Get the files.
+         */
+        getFiles(): void
+        {
+            this.loading = true;
 
-                if (this.searchText.length) {
-                    parameters.search = this.searchText;
-                }
+            this._getEndpoint
+                .then((response: any) => {
+                    this.loading = false;
 
-                return apiEndpoint
-                    .setParameters(parameters)
-                    .get();
+                    this.files = response.body.data;
+                    this.pagination = Pagination.makeFromResponse(response);
+                });
+        }
+
+        /**
+         * Perform the search.
+         */
+        search(): void
+        {
+            if (timeout) {
+                clearTimeout(timeout);
             }
-        },
 
-        methods: {
-
-            /**
-             * Get the files.
-             */
-            getFiles()
-            {
-                this.loading = true;
-
-                this._getEndpoint
-                    .then(response =>
-                    {
-                        this.loading = false;
-
-                        this.files = response.body.data;
-                        this.pagination = Pagination.makeFromResponse(response);
-                    });
-            },
-
-            /**
-             * Perform the search.
-             */
-            search()
-            {
-                if (timeout) {
-                    clearTimeout(timeout);
-                }
-
-                timeout = setTimeout(() =>
-                {
-                    this.getFiles();
-                }, 500);
-            },
-
-            /**
-             * Toggle file selection.
-             *
-             * @param file {Object}
-             */
-            toggleFile(file)
-            {
-                let foundFile = this.selected.find(current => current.id === file.id);
-
-                if (foundFile) {
-                    this.selected.splice(this.selected.indexOf(foundFile), 1);
-                } else {
-                    this.selected.push(file);
-                }
-            },
-
-            /**
-             * Update the pagination.
-             *
-             * @param pagination
-             */
-            updatePagination(pagination)
-            {
-                this.pagination = pagination;
+            timeout = setTimeout(() => {
                 this.getFiles();
-            }
-        },
+            }, 500);
+        }
 
-        mounted()
+        /**
+         * Toggle file selection.
+         *
+         * @param file {Object}
+         */
+        toggleFile(file: any): void
+        {
+            let foundFile = this.selected.find(current => current.id === file.id);
+
+            if (foundFile) {
+                this.selected.splice(this.selected.indexOf(foundFile), 1);
+            } else {
+                this.selected.push(file);
+            }
+        }
+
+        /**
+         * Update the pagination.
+         *
+         * @param pagination
+         */
+        updatePagination(pagination: any): void
+        {
+            this.pagination = pagination;
+            this.getFiles();
+        }
+
+        mounted(): void
         {
             this.getFiles();
-        },
+        }
 
-        watch: {
-
-            searchText: 'search'
+        @Watch('searchText')
+        onSearchTextChanged(): void
+        {
+            this.search();
         }
     }
+
+    export default FileSelect;
 
 </script>
