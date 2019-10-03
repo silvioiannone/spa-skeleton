9acb9c4eda43e6b5cb982473eba694c2c41c2843
Silvio Iannone <silvio@bloom.se>
2019-10-03 18:00:31 +0200

    Allow specifying the position key index in the Draggable component

--- src/Components/Draggable.vue
+++ src/Components/Draggable.vue
@@ -1,11 +1,13 @@
 <template>
-    <vue-draggable v-model="model" @change="moveItem" v-bind="options" class="handle">
+    <vue-draggable v-model="model" @change="moveItem" v-bind="options" :handle="handle"
+                   :class="{handle: ! handle.length}">
         <slot></slot>
     </vue-draggable>
 </template>
 
 <script lang="ts">
 
+    import _                        from 'lodash';
     import { Component, Prop, Vue } from 'vue-property-decorator';
     import VueDraggable             from 'vuedraggable';
 
@@ -16,6 +18,17 @@
     })
     export class Draggable extends Vue
     {
+        /**
+         * Class that will be used to identify the handle.
+         */
+        @Prop({ type: String, default: '' }) handle: Array<any>;
+
+        /**
+         * Key that denotes the key holding the position index. It can be a string with dot-sperated
+         * keys.
+         */
+        @Prop({ type: String, default: 'position' }) positionKey: string;
+
         /**
          * The items that will be ordered.
          */
@@ -49,9 +62,9 @@
          */
         moveItem(movement: any): void
         {
-            movement        = movement.moved;
-            let item        = movement.element;
-            let oldPosition = item.position;
+            movement         = movement.moved;
+            let item         = movement.element;
+            let oldPosition  = this.getPosition(item);
 
             // Get the position of the nearest item. The nearest item is the previous item if
             // moving the items down or the next item if moving them down.
@@ -59,7 +72,7 @@
 
             // Give the current item the same position as the reference item (the backend will
             // take care of updating the other item's position to the correct value).
-            item.position = nearestItem.position;
+            this.setPosition(item, this.getPosition(nearestItem));
 
             // Update the item.
             this.$emit('change', item);
@@ -67,25 +80,45 @@
             // Update the other items position by replicating the changes that are made
             // server-side.
             // If the item was moved down...
-            if (item.position >= oldPosition) {
+            if (this.getPosition(item) >= oldPosition) {
                 // Get all the previous items and decrease their position by one.
                 this.model
-                    .filter((_item: any) => {
-                        return (_item.position > oldPosition)
-                            && (_item.position <= item.position)
-                            && (_item.id !== item.id);
+                    .filter((currentItem: any): boolean => {
+                        return (this.getPosition(currentItem) > oldPosition)
+                            && (this.getPosition(currentItem) <= this.getPosition(item))
+                            && (currentItem.id !== item.id);
                     })
-                    .map((_item: any) => _item.position--);
+                    .forEach((currentItem: any) => {
+                        this.setPosition(currentItem, this.getPosition(currentItem) - 1);
+                    });
             } else {
                 // Get all the following items and increase their position by one.
                 this.model
-                    .filter((_item: any) => {
-                        return (_item.position >= item.position)
-                            && (_item.position < oldPosition)
-                            && (_item.id !== item.id);
+                    .filter((currentItem: any): boolean => {
+                        return (this.getPosition(currentItem) >= this.getPosition(item))
+                            && (this.getPosition(currentItem) < oldPosition)
+                            && (currentItem.id !== item.id);
                     })
-                    .map((_item: any) => _item.position++);
+                    .forEach((currentItem: any) => {
+                        this.setPosition(currentItem, this.getPosition(currentItem) + 1);
+                    });
             }
+        }
+
+        /**
+         * Get the position of an item.
+         */
+        getPosition(item: any): number
+        {
+            return _.get(item, this.positionKey);
+        }
+
+        /**
+         * Set the position of an item.
+         */
+        setPosition(item: any, position: number)
+        {
+            return _.set(item, this.positionKey, position);
         }
     }
 
