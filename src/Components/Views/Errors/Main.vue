1b0d6dd43883c92c8907edc4d8246e745fbd6fad
Silvio Iannone <silvio@bloom.se>
2019-10-29 12:59:33 +0100

    Update the main error view

--- src/Components/Views/Errors/Main.vue
+++ src/Components/Views/Errors/Main.vue
@@ -6,7 +6,7 @@
                     <v-layout wrap>
                         <v-flex xs12>
                             <div class="text-center mb-5">
-                                <v-icon x-large>{{ icon }}</v-icon>
+                                <v-icon x-large color="error">{{ icon }}</v-icon>
                             </div>
                             <h1 class="text-center display-2">{{ title }}</h1>
                         </v-flex>
@@ -17,44 +17,43 @@
                     </v-layout>
                 </v-container>
             </v-flex>
-            <v-flex xs12>
+            <v-flex xs12 v-if="backButton">
                 <div class="text-center">
-                    <v-btn color="primary" large outlined @click="goBack()" v-if="backButton">
+                    <v-btn color="primary" large outlined @click="goBack()" >
                         <v-icon left>arrow_back</v-icon>{{$t('misc.goBack')}}
                     </v-btn>
                 </div>
             </v-flex>
-        </v-layout>
-        <v-layout wrap v-if="app.config.env !== 'production' && error && error.body.message">
-            <v-flex xs12 class="my-5">
-                <v-divider></v-divider>
-            </v-flex>
-            <v-flex xs12>
-                <v-expansion-panel>
-                    <v-expansion-panel-content>
-                        <div slot="header">{{$t('misc.details')}}</div>
-                        <v-card>
-                            <v-card-title class="headline">
-                                <span v-if="error.status">{{ error.status }}</span>
-                                - {{ error.body.message }}
-                            </v-card-title>
-                            <v-card-text>
-                                <h5>
-                                    {{ $t('misc.onLine') }}
-                                    <span class="monospaced">{{ error.body.line }}</span>
-                                    {{ $t('misc.in') }}
-                                    <span class="monospaced">{{ error.body.file }}</span>.
-                                </h5>
-                            </v-card-text>
-                            <v-card-text class="monospaced">
+            <template>
+                <v-flex xs12 v-if="app.config.env !== 'production' && error && error.body.message">
+                    <v-expansion-panels>
+                        <v-expansion-panel>
+                            <v-expansion-panel-content>
+                                <div slot="header">{{$t('misc.details')}}</div>
+                                <v-card>
+                                    <v-card-title class="headline">
+                                        <span v-if="error.status">{{ error.status }}</span>
+                                        - {{ error.body.message }}
+                                    </v-card-title>
+                                    <v-card-text>
+                                        <h5>
+                                            {{ $t('misc.onLine') }}
+                                            <span class="monospaced">{{ error.body.line }}</span>
+                                            {{ $t('misc.in') }}
+                                            <span class="monospaced">{{ error.body.file }}</span>.
+                                        </h5>
+                                    </v-card-text>
+                                    <v-card-text class="monospaced">
                                 <pre>
                                     <code>{{ error.body.trace }}</code>
                                 </pre>
-                            </v-card-text>
-                        </v-card>
-                    </v-expansion-panel-content>
-                </v-expansion-panel>
-            </v-flex>
+                                    </v-card-text>
+                                </v-card>
+                            </v-expansion-panel-content>
+                        </v-expansion-panel>
+                    </v-expansion-panels>
+                </v-flex>
+            </template>
         </v-layout>
     </v-container>
 </template>
