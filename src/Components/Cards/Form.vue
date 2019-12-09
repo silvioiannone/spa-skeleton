50e1edd6d954faf3bc53f5fa0d3d852f28a5ea1a
Silvio Iannone <silvio@bloom.se>
2019-12-09 11:54:22 +0100

    Allow a `card-form` display a close icon

--- src/Components/Cards/Form.vue
+++ src/Components/Cards/Form.vue
@@ -1,19 +1,27 @@
 <template>
     <card-main v-on="getOn()" v-bind="getProps()">
-        <v-card-text>
-            <slot :bubble-cancel="bubbleCancel" :bubble-submit="bubbleSubmit"></slot>
-        </v-card-text>
-        <v-card-actions v-if="$slots.actions">
-            <slot name="actions"></slot>
-        </v-card-actions>
+        <template #title-actions>
+            <slot name="title-actions"></slot>
+            <v-btn v-if="closable" icon @click="$emit('cancel')">
+                <v-icon>mdi-close</v-icon>
+            </v-btn>
+        </template>
+        <template #default>
+            <v-card-text>
+                <slot :bubble-cancel="bubbleCancel" :bubble-submit="bubbleSubmit"></slot>
+            </v-card-text>
+            <v-card-actions v-if="$slots.actions">
+                <slot name="actions"></slot>
+            </v-card-actions>
+        </template>
     </card-main>
 </template>
 
 <script lang="ts">
 
-    import { Component, Mixins } from 'vue-property-decorator';
-    import { Wrapper }           from '../Mixins/Wrapper.vue';
-    import { CardMain }          from './Main.vue';
+    import { Component, Mixins, Prop } from 'vue-property-decorator';
+    import { Wrapper } from '../Mixins/Wrapper.vue';
+    import { CardMain } from './Main.vue';
 
     @Component({
         components: {
@@ -22,6 +30,11 @@
     })
     export class CardForm extends Mixins(Wrapper)
     {
+        /**
+         * Display a close button.
+         */
+        @Prop({ type: Boolean, default: false }) closable: boolean;
+
         __component = CardMain;
 
         /**
