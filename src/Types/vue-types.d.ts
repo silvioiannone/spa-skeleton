57c3aeac4e0f9c09c598f0d49ffb3b9b32135b29
Silvio Iannone <silvio@bloom.se>
2021-11-24 10:25:31 +0100

    Update ABFast screen template

---
+++ src/Types/vue-types.d.ts
@@ -0,0 +1,44 @@
+import Router, { Route }       from 'vue-router';
+import { VuetifyObject }       from 'vuetify';
+import { Store }               from 'vuex';
+import VueI18n, { IVueI18n }   from 'vue-i18n';
+import { ErrorBag, Validator } from 'vee-validate';
+import Config                  from '../Config';
+import { ApiClient }           from '../Library/Api';
+import { WebSocket }           from '../Library/WebSocket';
+import { Navigator }           from '../Library/Utils/Navigator';
+import { Head }                from '../Library/Utils/Head';
+import { String }              from '../Library/Utils/String';
+import { Toolbar }             from '../Library/Utils/Ui/Toolbar';
+
+declare module 'vue/types/vue'
+{
+    interface Vue
+    {
+        readonly $api: ApiClient;
+        readonly $i18n: VueI18n & IVueI18n;
+        $t: typeof VueI18n.prototype.t;
+        $tc: typeof VueI18n.prototype.tc;
+        $te: typeof VueI18n.prototype.te;
+        $d: typeof VueI18n.prototype.d;
+        $n: typeof VueI18n.prototype.n;
+        $eh: Vue;
+        $config: Config;
+        $head: typeof Head;
+        $navigator: Navigator;
+        $route: Route;
+        $router: Router;
+        $store: Store<any>;
+        $vuetify: VuetifyObject;
+        $validator: Validator;
+        $ui: {
+            toolbar: typeof Toolbar
+        };
+        $user: any,
+        $utils: {
+            string: typeof String
+        };
+        $ws: WebSocket;
+        errors: ErrorBag;
+    }
+}
